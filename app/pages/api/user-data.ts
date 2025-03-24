d5a44cbca12828d494e98631cbbbff3cd671ed96
Keshia Dawn Saguilayan <saguilayan.keshiadawn21@gmail.com>
2025-03-24 11:34:04 +0800

    updated supabase url with next_public_

--- app/pages/api/user-data.ts
+++ app/pages/api/user-data.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client (server-side only)
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY! // Never expose this key on the frontend!
 );
 
